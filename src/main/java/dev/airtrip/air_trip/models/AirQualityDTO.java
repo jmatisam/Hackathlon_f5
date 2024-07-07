4062c2d67ad489eb5073ef0b0a40d6724baeda8b
jmat732 <jmatisamuel@gmail.com>
2024-07-07 20:55:28 +0200

    dto

--- src/main/java/dev/airtrip/air_trip/models/AirQualityDTO.java
+++ src/main/java/dev/airtrip/air_trip/models/AirQualityDTO.java
@@ -20,3 +20,12 @@ public class AirQualityDTO {
     }
 
 }
+/*Un DTO (Data Transfer Object) es una clase que se utiliza para transferir datos entre diferentes capas
+ de una aplicación, especialmente entre el backend y el frontend.
+
+*Mapear datos de una entidad a un DTO (Data Transfer Object) tiene varias ventajas importantes:
+
+Seguridad: Permite enviar solo los datos necesarios al frontend, evitando exponer información sensible o innecesaria.
+Optimización: Reduce la cantidad de datos transferidos, mejorando el rendimiento de la aplicación.
+Desacoplamiento: Separa la lógica de negocio de la presentación, facilitando el mantenimiento y la evolución del código.
+Validación: Facilita la validación de datos antes de enviarlos al frontend, asegurando que solo se envíen datos correctos y completos */
